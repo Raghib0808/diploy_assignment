fc1e73bde64e268161e26aa34a2967a870cdd631
Raghib <rahgib@gmail.com>
2024-09-24 01:21:22 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { useEffect, useState } from 'react';
 import Body from './components/Body';
 import Header from './components/Header';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Slider.css';
 import Loader from './components/Loader';
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate DOMContentLoaded by using useEffect
-    const handleLoad = () => {
-      setLoading(false);  // Remove the loader once content is loaded
+    const handleLoaded = () => {
+      setIsLoading(false);
     };
 
-    // Mimic DOMContentLoaded behavior
-    window.addEventListener('load', handleLoad);
+    if (document.readyState === 'complete') {
+      handleLoaded();
+    } else {
+      window.addEventListener('load', handleLoaded);
+    }
 
-    // Cleanup function
-    return () => window.removeEventListener('load', handleLoad);
+    return () => {
+      window.removeEventListener('load', handleLoaded);
+    };
   }, []);
 
-  // Render the Loader when the content is loading
-  if (loading) {
+  if (isLoading) {
     return <Loader />;
   }
 
