765b7c6c023d87e7920acf282265766fcc1de289
Raghib <rahgib@gmail.com>
2024-09-24 01:11:56 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import Body from './components/Body';
 import Header from './components/Header';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 import './Slider.css';
+import Loader from './components/Loader';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate DOMContentLoaded by using useEffect
+    const handleLoad = () => {
+      setLoading(false);  // Remove the loader once content is loaded
+    };
+
+    // Mimic DOMContentLoaded behavior
+    window.addEventListener('load', handleLoad);
+
+    // Cleanup function
+    return () => window.removeEventListener('load', handleLoad);
+  }, []);
+
+  // Render the Loader when the content is loading
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className="App">
-      <Header/>
-      <Body/>
+      <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Container_loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Container_loader = () => {
+  return (
+    <div className='loader-container'>
+      
+    </div>
+  )
+}
+
+export default Container_loader
